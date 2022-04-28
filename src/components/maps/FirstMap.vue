0872be38ebf0dd2399489e03493ea93280310294
ang liu <jackyclearliu@gmail.com>
2022-04-28 23:47:58 +0800

    maptalks示例1

--- src/components/maps/FirstMap.vue
+++ src/components/maps/FirstMap.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="base-map" :style="{ width: width, height: height, 'z-index': zIndex }">
+  <div id="base-map" class="base-map" :style="{ width: width, height: height, 'z-index': zIndex }">
 
   </div>
 </template>
 
 <script>
-import { getCurrentInstance, onMounted, ref } from "vue"
+import { getCurrentInstance, nextTick, onMounted, ref } from "vue"
 export default {
   name: 'firstMap',
   props: {
     width: {
       type: String,
-      deault: '100%'
+      default: '100%'
     },
     height: {
       type: String,
@@ -19,15 +19,32 @@ export default {
     },
     zIndex: {
       type: [String, Number],
-      default: '1'
+      default: '0'
     }
   },
-  setup(props) {
+  setup() {
     // 初始化
     const instance = getCurrentInstance()
     const _this = instance.proxy
     const global = instance.appContext.config.globalProperties
+    // const map = ref(null)
 
+    const initMap = (mapName) => {
+      let myMap = new global.maptalks.Map(mapName, {
+        center: [-0.113049, 51.498568],
+        zoom: 14,
+        baseLayer: new global.maptalks.TileLayer('white-map', {
+          urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+          subdomains: ['a', 'b', 'c', 'd'],
+          attribution: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
+        })
+      })
+      return myMap
+    }
+    onMounted(async () => {
+      await nextTick()
+      initMap('base-map')
+    })
 
     return {}
   }
